75e68827591b13a9556748fc026729b58c05b35a
doohong <wnghd94@gmail.com>
2021-03-13 22:51:39 +0900

    git init

---
+++ src/main/kotlin/com/doohong/oauth/config/AuthServerConfig.kt
@@ -0,0 +1,11 @@
+package com.doohong.oauth.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+
+@Configuration
+@EnableAuthorizationServer
+class AuthServerConfig : AuthorizationServerConfigurerAdapter() {
+}
+
