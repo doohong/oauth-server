5d0d22d09a79e1b1f1436a5f23e391b2f8fc305e
doohong <wnghd94@gmail.com>
2021-03-16 23:27:36 +0900

    db 정보로 인증서버 기동

--- src/main/kotlin/com/doohong/oauth/config/AuthServerConfig.kt
+++ src/main/kotlin/com/doohong/oauth/config/AuthServerConfig.kt
@@ -1,21 +1,28 @@
 package com.doohong.oauth.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import javax.sql.DataSource
 
 @Configuration
 @EnableAuthorizationServer
-class AuthServerConfig : AuthorizationServerConfigurerAdapter() {
+class AuthServerConfig(
+    private val passwordEncoder: PasswordEncoder,
+    private val dataSource: DataSource
+) : AuthorizationServerConfigurerAdapter() {
     override fun configure(clients: ClientDetailsServiceConfigurer) {
-        clients.inMemory()
-                .withClient("testClientId")
-                .secret("{noop}testSecret")
-                .redirectUris("http://localhost:8081/oauth2/callback")
-                .authorizedGrantTypes("authorization_code")
-                .scopes("read", "write")
-                .accessTokenValiditySeconds(30000);
+        clients.jdbc(dataSource).passwordEncoder(passwordEncoder)
+//        clients.inMemory()
+//                .withClient("testClientId")
+//                .secret("{noop}testSecret")
+//                .redirectUris("http://localhost:8081/oauth2/callback")
+//                .authorizedGrantTypes("authorization_code")
+//                .scopes("read", "write")
+//                .accessTokenValiditySeconds(30000);
     }
 }
 
