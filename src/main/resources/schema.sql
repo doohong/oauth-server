e3fceb28101f7408240e1bc5c8fd619e71b16ea8
doohong <wnghd94@gmail.com>
2021-03-18 23:56:26 +0900

    oauth 초기 데이터 sql 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,6 @@ create table IF NOT EXISTS oauth_client_details (
     additional_information VARCHAR(4096),
     autoapprove VARCHAR(256)
 );
+
+insert into oauth_client_details(client_id, resource_ids,client_secret,scope,authorized_grant_types,web_server_redirect_uri,authorities,access_token_validity,refresh_token_validity,additional_information,autoapprove)
+values('testClientId',null,'{bcrypt}$2a$10$H2oQgFY7qCRHWqkvAV4P6ONy2v74wfr3fQv.xERw3BJYSqh/Gcgrq','read,write','authorization_code,refresh_token','http://localhost:8081/oauth2/callback','ROLE_USER',36000,50000,null,null);
