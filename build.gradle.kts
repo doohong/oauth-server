50bf3422166342b9c445741ed4b1cdb67092d378
doohong <wnghd94@gmail.com>
2021-04-18 19:57:56 +0900

    gradle version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.2.6.RELEASE" apply false
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.springframework.boot") version "2.3.4.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
     kotlin("plugin.jpa") version "1.3.71"
